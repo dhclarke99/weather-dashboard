c83505d978cfb1d915d1104bca363ad83f2e0478
dhclarke99 <dhclarke99@gmail.com>
2023-04-03 22:28:42 -0400

    added weather attributes to main display

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@
 var apiKey = "1c39f5ca13c985d309cd5081d8f2b159";
 var button = document.querySelector('.btn')
 var cityName = document.querySelector("#search-input-sidenav")
+var currentCity = document.querySelector(".col-12")
+var dayOne = document.querySelector("#day-1")
+var dayTwo = document.querySelector("#day-2")
+var dayThree = document.querySelector("#day-3")
+var dayFour = document.querySelector("#day-4")
+var dayFive = document.querySelector("#day-5")
 
 function getCityName() {
     console.log(cityName.value)
@@ -23,9 +29,9 @@ function getCityName() {
     var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=1c39f5ca13c985d309cd5081d8f2b159";
     
     fetch(requestUrl)
-      .then(function (response) {
+      .then(function (getWeatherData) {
       
-        return response.json();
+        return getWeatherData.json();
         
         
       })
@@ -38,9 +44,28 @@ function getCityName() {
         currentTemp = (data.list[0].main.temp)
         currentHumidity = (data.list[0].main.humidity)
         currentWind = (data.list[0].wind.speed)
+      
+
+        setCurrentWeather()
+       function setCurrentWeather() {
+        console.log(cityName.value)
+        console.log(dateFormat)
         console.log(currentTemp)
         console.log(currentHumidity)
         console.log(currentWind)
+        console.log(currentCity)
+        currentCity.children[0].textContent = (cityName.value + " " + dateFormat)
+        var item1 = document.querySelector(".temp")
+        var item2 = document.querySelector(".wind")
+        var item3 = document.querySelector(".humidity")
+      
+       
+        item1.textContent = ("Temp: " + currentTemp + " degrees Farenheit")
+        item2.textContent = ("Wind: " + currentWind + " MPH ")
+        item3.textContent = ("Humidity: " + currentHumidity + " % ")
+
+        }
+
 
         }
       );
